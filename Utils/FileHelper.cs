f350bfb4a75feceb2a223863465ea9ed1e449bd7
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-11-20 13:04:53 -0300

    Add FileHelper

---
+++ Utils/FileHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace YoutubeVideoDownloader.Utils
+{
+    public static class FileHelper
+    {
+        public static string CreateDirectory(string folderName)
+        {
+            var path = Path.Combine(@"C:\Users", Environment.UserName, "Videos", folderName);
+            var directoryDoesNotExist = !Directory.Exists(path);
+            if (directoryDoesNotExist)
+            {
+                Directory.CreateDirectory(path);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Folder created!");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Folder already exists!");
+                Console.ResetColor();
+            }
+            return path;
+        }
+
+    }
+}
